aab3b90e5c180b16115bbfe6beb27a890732f1b9
shivg03 <shivgadway05@gmail.com>
2024-04-04 14:25:39 +0530

    new file:   .env 	new file:   README.md 	new file:   carshowcase_back/db.js 	new file:   carshowcase_back/models/bookingModel.js 	new file:   carshowcase_back/models/carModel.js 	new file:   carshowcase_back/models/userModel.js 	new file:   carshowcase_back/routes/bookingsRoute.js 	new file:   carshowcase_back/routes/carsRoute.js 	new file:   carshowcase_back/routes/usersRoute.js 	new file:   carshowcase_back/server.js 	new file:   carshowcase_front/build/asset-manifest.json 	new file:   carshowcase_front/build/download.svg 	new file:   carshowcase_front/build/favicon.ico 	new file:   carshowcase_front/build/favicon.svg 	new file:   carshowcase_front/build/index.html 	new file:   carshowcase_front/build/logo192.png 	new file:   carshowcase_front/build/logo512.png 	new file:   carshowcase_front/build/manifest.json 	new file:   carshowcase_front/build/robots.txt 	new file:   carshowcase_front/build/static/css/2.63d66c9b.chunk.css 	new file:   carshowcase_front/build/static/css/2.63d66c9b.chunk.css.map 	new file:   carshowcase_front/build/static/css/main.073ed592.chunk.css 	new file:   carshowcase_front/build/static/css/main.073ed592.chunk.css.map 	new file:   carshowcase_front/build/static/js/2.4bfe5d34.chunk.js 	new file:   carshowcase_front/build/static/js/2.4bfe5d34.chunk.js.LICENSE.txt 	new file:   carshowcase_front/build/static/js/2.4bfe5d34.chunk.js.map 	new file:   carshowcase_front/build/static/js/3.67331c36.chunk.js 	new file:   carshowcase_front/build/static/js/3.67331c36.chunk.js.map 	new file:   carshowcase_front/build/static/js/main.c2a190e4.chunk.js 	new file:   carshowcase_front/build/static/js/main.c2a190e4.chunk.js.map 	new file:   carshowcase_front/build/static/js/runtime-main.2913e8d0.js 	new file:   carshowcase_front/build/static/js/runtime-main.2913e8d0.js.map 	new file:   carshowcase_front/build/static/media/logo.6ce24c58.svg 	new file:   carshowcase_front/package-lock.json 	new file:   carshowcase_front/package.json 	new file:   carshowcase_front/public/download.svg 	new file:   carshowcase_front/public/favicon.ico 	new file:   carshowcase_front/public/favicon.svg 	new file:   carshowcase_front/public/index.html 	new file:   carshowcase_front/public/logo192.png 	new file:   carshowcase_front/public/logo512.png 	new file:   carshowcase_front/public/manifest.json 	new file:   carshowcase_front/public/robots.txt 	new file:   carshowcase_front/src/App.css 	new file:   carshowcase_front/src/App.js 	new file:   carshowcase_front/src/App.test.js 	new file:   carshowcase_front/src/components/DefaultLayout.js 	new file:   carshowcase_front/src/components/Spinner.js 	new file:   carshowcase_front/src/components/defaultlayout.css 	new file:   carshowcase_front/src/index.css 	new file:   carshowcase_front/src/index.js 	new file:   carshowcase_front/src/logo.svg 	new file:   carshowcase_front/src/pages/AddCar.js 	new file:   carshowcase_front/src/pages/AdminHome.js 	new file:   carshowcase_front/src/pages/BookingCar.js 	new file:   carshowcase_front/src/pages/EditCar.js 	new file:   carshowcase_front/src/pages/Home.js 	new file:   carshowcase_front/src/pages/Login.js 	new file:   carshowcase_front/src/pages/Register.js 	new file:   carshowcase_front/src/pages/UserBookings.js 	new file:   carshowcase_front/src/pages/home.css 	new file:   carshowcase_front/src/redux/actions/bookingActions.js 	new file:   carshowcase_front/src/redux/actions/carsActions.js 	new file:   carshowcase_front/src/redux/actions/userActions.js 	new file:   carshowcase_front/src/redux/reducers/alertsReducer.js 	new file:   carshowcase_front/src/redux/reducers/bookingsReducer.js 	new file:   carshowcase_front/src/redux/reducers/carsReducer.js 	new file:   carshowcase_front/src/redux/store.js 	new file:   carshowcase_front/src/reportWebVitals.js 	new file:   carshowcase_front/src/setupTests.js 	new file:   package-lock.json 	new file:   package.json

---
+++ carshowcase_back/db.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+require('dotenv').config();
+// console.log(process.env.MONGOURL)
+
+function connectDB(){
+
+    mongoose.connect(process.env.MONGOURL , {useUnifiedTopology: true , useNewUrlParser: true})
+
+    const connection = mongoose.connection
+
+    connection.on('connected' , ()=>{
+        console.log('Mongo DB Connection Successfull')
+    })
+
+    connection.on('error' , ()=>{
+        console.log('Mongo DB Connection Error')
+    })
+
+}
+
+connectDB()
+
+module.exports = mongoose
